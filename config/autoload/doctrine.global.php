dd0d2de228a7f868102d7d0b583f0cfafd57072a
Evgeny Shpilevsky <zhekanot@gmail.com>
2013-07-05 15:13:18 +0300

    Doctrine 0.8.x-dev

---
+++ config/autoload/doctrine.global.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host' => 'localhost',
+                    'port' => '3306',
+                    'user' => '',
+                    'password' => '',
+                    'dbname' => '',
+                    'charset' => 'utf8',
+                    'driverOptions' => array(
+                        1002 => 'SET NAMES utf8'
+                    )
+                ),
+            )
+        ),
+        'configuration' => array(
+            'orm_default' => array(
+                'metadata_cache' => 'my_memcache',
+                'query_cache' => 'my_memcache',
+                'result_cache' => 'my_memcache',
+            )
+        ),
+        // migrations configuration
+//        'migrations_configuration' => array(
+//            'orm_default' => array(
+//                'directory' => 'data/DoctrineORMModule/Migrations',
+//                'namespace' => 'DoctrineORMModule\Migrations',
+//                'table' => 'migrations',
+//            ),
+//        ),
+    )
+);
